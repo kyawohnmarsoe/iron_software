82e8e502fc6ee9506b952bb828926d2a29b9896a
Kyaw Ohn Mar Soe <kyawohnmarsoe@outlook.com>
2023-04-27 12:51:07 +0400

    update observer

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+window.addEventListener('DOMContentLoaded', setup); 
+
+function setup() {
+    const options = {
+        rootMargin: '0px 0px -200px 0px'
+    }
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if(entry.isIntersecting) {
+                entry.target.classList.add('show');
+                observer.unobserve(entry.target);
+            } else {
+                return;
+            }
+        })
+    }, options);
+
+   
+    const paras = document.querySelectorAll('.to-observe');
+    paras.forEach(p => observer.observe(p));
+}
